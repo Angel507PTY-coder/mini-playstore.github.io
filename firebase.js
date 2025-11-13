2c5aaf49854b8954a618f4ca384a5406bbdbcb2c
Angel507PTY-coder <angelsanchezx82@gmail.com>
2025-11-13 17:01:26 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,26 @@
+// Importar funciones de Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getFirestore, collection, getDocs, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
+// Configuración de tu proyecto Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDoizr-HCNDNaklLmBgTGqfvRVukzfxiMQ",
+  authDomain: "mini-playstore.firebaseapp.com",
+  projectId: "mini-playstore",
+  storageBucket: "mini-playstore.firebasestorage.app",
+  messagingSenderId: "614694898128",
+  appId: "1:614694898128:web:7eac3954c8887993a851d5"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Iniciar sesión anónima (permite subir apps)
+signInAnonymously(auth).then(() => {
+  console.log("Sesión anónima iniciada ✅");
+}).catch((error) => console.error("Error al iniciar sesión:", error));
+
+export { db, collection, getDocs, addDoc, serverTimestamp };
